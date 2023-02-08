c73ef6bc7b8512fb970b37b07d3d218616b4239f
PriyanshShrivastava <learn.priyansh@gmail.com>
2023-02-08 17:02:24 +0530

    docs : code files added

---
+++ main.js
@@ -0,0 +1,23 @@
+const inputEl = document.querySelector(".qr-input");
+const imgEl = document.querySelector(".img-container");
+const img = document.querySelector("#qr-img");
+const generatorBtn = document.querySelector(".generator-btn");
+const retryBtn = document.querySelector("#retry");
+
+const qrGenerator = function () {
+  img.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputEl.value}`;
+
+  setTimeout(function () {
+    imgEl.style.maxHeight = "200px";
+  }, 200);
+
+  inputEl.value = ``;
+};
+
+generatorBtn.addEventListener("click", qrGenerator);
+retryBtn.addEventListener("click", function () {
+  setTimeout(function () {
+    imgEl.style.maxHeight = "0";
+    img.src = ``;
+  }, 600);
+});
